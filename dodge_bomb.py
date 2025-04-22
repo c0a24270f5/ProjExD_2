999e1d5881b569c5883ce75f65dc0217cbebf025
yuki <c0a24270f5@edu.teu.ac.jp>
2025-04-22 14:50:12 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,23 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：タプル（横方向判定結果、縦方向判定結果）
+    画面内ならTrue, 画面外ならFalse
+    """
+
+    yoko, tate = True, True
+    if  rct.left < 0 or WIDTH < rct.right:       #画面外であったら
+        yoko = False
+    if rct.top < 0  or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
+
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -45,8 +62,17 @@ def main():
                 sum_mv[1] += mv[1]
                 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])   #画面の外だったら
+        
         bb_rct.move_ip(vx, vy)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
