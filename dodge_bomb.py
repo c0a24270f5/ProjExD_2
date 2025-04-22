5376da26482a7541aa9795d473ac386af8abd37f
yuki <c0a24270f5@edu.teu.ac.jp>
2025-04-22 15:03:07 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,9 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct): #こうかとんRectと爆弾Rectが重なっていたら
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
 
